6110e7962c1690e53f66e2ec6976bc173c695b71
tala <s12219511@stu.najah.edu>
2025-01-05 23:52:08 +0000

    project v1

--- info.php
+++ info.php
@@ -1,4 +1,28 @@
 <?php
-phpinfo();
-echo "hi";
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "dictionary_app";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_GET['word'])) {
+    $word = $conn->real_escape_string($_GET['word']);
+
+    $sql = "SELECT machine_word FROM words WHERE input_word='$word'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo $row['machine_word'];
+    } else {
+        echo "Word not found";
+    }
+}
+
+$conn->close();
 ?>
