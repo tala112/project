f9e8252215c34fda2a032589ea91e60d722c6581
tala112 <s12219511@stu.najah.edu>
2025-01-14 00:17:35 +0000

    wrong php again 2!

--- info.php
+++ info.php
@@ -1,5 +1,4 @@
 <?php
-/*
 $servername = "127.0.0.1";
 $username = "root";
 $password = "password";
@@ -8,7 +7,9 @@ $dbname = "dictionary_app";
 $conn = new mysqli($servername, $username, $password, $dbname);
 //if con secsesful
 if ($conn->connect_error) {
-    die("sorry Connection failed: " . $conn->connect_error);
+    $conn = new mysqli("192.168.88.12", $username, $password, $dbname);
+    if ($conn->connect_error)
+    {die("sorry Connection failed: " . $conn->connect_error);}
 }
 
 if (isset($_GET['word'])) {
@@ -26,32 +27,4 @@ if (isset($_GET['word'])) {
 }
 
 $conn->close();
-*/
-$host_ip = "192.168.88.12"; // عنوان IP الخاص بالنظام المضيف
-$local_ip = "127.0.0.1";    // عنوان localhost
-$username = "root";
-$password = "password";
-$dbname = "dictionary_app";
-
-// وظيفة لإنشاء الاتصال
-function connectToDatabase($servername, $username, $password, $dbname) {
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        return false; // فشل الاتصال
-    }
-    return $conn; // اتصال ناجح
-}
-
-// جرب الاتصال باستخدام IP المضيف
-$conn = connectToDatabase($host_ip, $username, $password, $dbname);
-
-// إذا فشل، جرب الاتصال بـ localhost
-if (!$conn) {
-    $conn = connectToDatabase($local_ip, $username, $password, $dbname);
-}
-
-// إذا فشل الاتصال في الحالتين، أوقف التنفيذ
-if (!$conn) {
-    die("Connection failed: Unable to connect to database using both host IP and localhost.");
-}
 ?>
